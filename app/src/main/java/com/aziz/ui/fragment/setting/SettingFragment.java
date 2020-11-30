e4fc9b08b6afa9a07b10828eb30af6e872fece6d
Kashkula <konoshov947@mail.ru>
2020-11-30 16:48:52 +0600

    with architecture

--- app/src/main/java/com/aziz/ui/fragment/setting/SettingFragment.java
+++ app/src/main/java/com/aziz/ui/fragment/setting/SettingFragment.java
@@ -1,38 +1,30 @@
 package com.aziz.ui.fragment.setting;
 
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProviders;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.aziz.App;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.aziz.R;
 import com.aziz.data.adapter.theme.OnClickSF;
 import com.aziz.data.adapter.theme.ThemeAdapter;
 import com.aziz.data.model.ThemeModel;
 import com.aziz.databinding.SettingFragmentBinding;
-import com.aziz.ui.activity.SplashActivity;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class SettingFragment extends Fragment implements OnClickSF {
 
-    private SettingViewModel vm;
+    protected SettingViewModel vm;
     protected SettingFragmentBinding binding;
     protected ArrayList<ThemeModel> list;
     protected ThemeAdapter adapter;
@@ -47,34 +39,21 @@ public class SettingFragment extends Fragment implements OnClickSF {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        vm = ViewModelProviders.of(this).get(SettingViewModel.class);
-    }
-
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SharedPreferences sP = App.sp;
-        sP.edit().putInt(THEME, 5).apply();
+        vm = ViewModelProviders.of(this).get(SettingViewModel.class);
+
+        vm.defaultTheme();
 
         init();
 
-        add("Red");
-        add("Orange");
-        add("Blue");
-        add("Dark");
-        add("Green");
-        add("Light");
+        vm.addList(adapter);
+        onClick();
+    }
 
-        binding.layout4.setOnClickListener(v -> {
-            clear();
-        });
-
-        binding.tvClear.setOnClickListener(v -> {
-            clear();
-        });
-
+    public void onClick() {
+        binding.layout4.setOnClickListener(v -> vm.clear(getContext()));
+        binding.tvClear.setOnClickListener(v -> vm.clear(getContext()));
     }
 
     private void init() {
@@ -83,16 +62,6 @@ public class SettingFragment extends Fragment implements OnClickSF {
         adapter = new ThemeAdapter(list, this, Objects.requireNonNull(getActivity()));
         binding.rv.setLayoutManager(manager);
         binding.rv.setAdapter(adapter);
-
-    }
-
-    public void clear() {
-        Toast.makeText(getContext(), "History: cleaned", Toast.LENGTH_SHORT).show();
-        App.db.quizDao().deleteAll();
-    }
-
-    public void add(String name) {
-        adapter.add(new ThemeModel(name, R.drawable.phone));
     }
 
     @Override
@@ -101,12 +70,10 @@ public class SettingFragment extends Fragment implements OnClickSF {
     }
 
     public void setTheme(int position) {
-        SharedPreferences sP = App.sp;
-        if (sP.getInt(SettingFragment.THEME, 20) != position) {
-            sP.edit().putInt(THEME, position).apply();
-            Objects.requireNonNull(getActivity()).startActivity(new Intent(getActivity(), SplashActivity.class));
-            getActivity().finish();
-        } else
-            Toast.makeText(getContext(), "Вы уже выбрали эту тему!", Toast.LENGTH_SHORT).show();
+        vm.setTheme(getContext(), position);
+        vm.booleanMutableLiveData.observe(getViewLifecycleOwner(), aBoolean -> {
+            if (aBoolean)
+                Objects.requireNonNull(getActivity()).finish();
+        });
     }
 }
